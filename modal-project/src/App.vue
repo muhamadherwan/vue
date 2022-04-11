3f1ef9fe54483e7d88cc0bcfd0d8cd56b2e1a281
muhamadherwan <hello.wanlab@gmail.com>
2022-04-11 15:25:17 +0800

    init modal-project

---
+++ modal-project/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <h1>{{ title }}</h1>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  data() {
+    return {
+      title: "My first vue app"
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+h1 {
+  border-bottom: 1px solid #ddd;
+  display: inline-block;
+  padding-bottom: 10px;
+}
+</style>

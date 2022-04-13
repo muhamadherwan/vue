288cb1b7e8b17e0726217916be8f83712cb364e3
muhamadherwan <hello.wanlab@gmail.com>
2022-04-13 11:39:23 +0800

    redirect and 404

--- vue-job/src/router/index.js
+++ vue-job/src/router/index.js
@@ -3,6 +3,7 @@ import HomeView from '../views/HomeView.vue'
 import About from '../views/AboutView.vue'
 import Jobs from '../views/jobs/Jobs.vue'
 import JobDetails from '../views/jobs/JobDetails.vue'
+import NotFound from '../views/NotFound.vue'
 
 const routes = [
   {
@@ -25,6 +26,17 @@ const routes = [
     name: 'JobDetails',
     component: JobDetails,
     props: true
+  },
+  // redirect
+  {
+    path: '/all-jobs',
+    redirect: '/jobs'
+  },
+  // catch all 404
+  {
+    path: '/:catchAll(.*)',
+    name: 'NotFound',
+    component: NotFound
   }
 ]
 

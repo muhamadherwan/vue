fc86dcb3949b097b34e7f4695e2c778d5ee11819
muhamadherwan <hello.wanlab@gmail.com>
2022-04-13 14:39:14 +0800

    reactive

--- dojo-blog/src/views/HomeView.vue
+++ dojo-blog/src/views/HomeView.vue
@@ -1,29 +1,33 @@
 <template>
   <div class="home">home</div>
-  <p>My name is {{ name }} and my age is {{ age }}</p>
-  <button @click="age++">Add age</button>
-  <button @click="handleClick">Click me</button>
-  <input type="text" v-model="name">
-
+  <h2>Ref</h2>
+  <p>{{ personOne.name }} - {{ personOne.age }}</p>
+  <button @click="updatePersonOne">Update person one</button>
+  <h2>Reactive</h2>
+  <p>{{ personTwo.name }} - {{ personTwo.age }}</p>
+  <button @click="updatePersonTwo">Update person two</button>
 </template>
 
 <script>
-import { ref } from '@vue/reactivity'
+import { reactive, ref } from '@vue/reactivity'
 
 export default {
   name: 'HomeView',
   // composation api function
   setup() {
+    const personOne = ref({ name: 'mario', age: 30})
 
-    const name = ref('mario')
-    const age = ref(30)
+    const personTwo = reactive({ name: 'ayu', age: 40})
 
-    const handleClick = () => {
-      name.value = 'Ayu'
-      age.value = 41
+    const updatePersonOne = () => {
+      personOne.value.age = 40
     }
 
-    return {name, age, handleClick }
+    const updatePersonTwo = () => {
+      personTwo.age = 50
+    }
+
+    return { personOne, updatePersonOne, personTwo, updatePersonTwo }
   }
 }
 </script>

7aefee0185491dc428f58b835e1620589e84d2e7
muhamadherwan <hello.wanlab@gmail.com>
2022-04-13 15:10:04 +0800

    search using computed values

--- dojo-blog/src/views/HomeView.vue
+++ dojo-blog/src/views/HomeView.vue
@@ -1,33 +1,27 @@
 <template>
   <div class="home">home</div>
-  <h2>Ref</h2>
-  <p>{{ personOne.name }} - {{ personOne.age }}</p>
-  <button @click="updatePersonOne">Update person one</button>
-  <h2>Reactive</h2>
-  <p>{{ personTwo.name }} - {{ personTwo.age }}</p>
-  <button @click="updatePersonTwo">Update person two</button>
+  <h2>Computed Value</h2>
+  <input type="text" v-model="search">
+  <p>search term - {{ search }}</p>
+  <div v-for="name in matchingNames" :key="name">
+    {{ name }}
+  </div>
 </template>
 
 <script>
-import { reactive, ref } from '@vue/reactivity'
+import { computed, ref } from '@vue/reactivity'
 
 export default {
   name: 'HomeView',
   // composation api function
   setup() {
-    const personOne = ref({ name: 'mario', age: 30})
+    const search = ref('')
+    const names = ref(['mario', 'yoshi', 'daigo', 'ezzi'])
 
-    const personTwo = reactive({ name: 'ayu', age: 40})
-
-    const updatePersonOne = () => {
-      personOne.value.age = 40
-    }
-
-    const updatePersonTwo = () => {
-      personTwo.age = 50
-    }
-
-    return { personOne, updatePersonOne, personTwo, updatePersonTwo }
+    const matchingNames = computed(() => {
+      return names.value.filter((name) => name.includes(search.value))
+    })
+    return { names, search, matchingNames }
   }
 }
 </script>
